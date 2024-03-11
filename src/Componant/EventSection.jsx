730648503e4444aabc040783281d9ba6f801b33b
yj-10 <infosense100@gmail.com>
2024-03-11 18:58:04 +0530

    update something

--- src/Componant/EventSection.jsx
+++ src/Componant/EventSection.jsx
@@ -5,9 +5,21 @@ const EventSection = () => {
     <>
       <div className="Event-section">
               <div className="Event-main">
-              <div className="event-first">
-                  <img src='	https://mediacity.co.in/delici/static/media/bg-8.11bc61489e11b03a92c1.png' />
+              <div className="event-inner">
+              <div className="event-box">
+              <div className="event-title">
+                              BEST MENU
               </div>
+                          <h1 className='eve-h'>Special Dishes</h1>
+                          <p className='even-pera'>Lorem Ipsum is simply dummy printing.</p>
+                          <div className='view-m'>
+                              <span>view menu</span>
+                          </div>       
+              </div>
+              <div className="event-first">
+                      <img src='		https://mediacity.co.in/delici/static/media/featured-1.a988d10de26c41476f26.jpg' />
+              </div>
+                  </div>
                   <div className="event-first">
                       <img src='		https://mediacity.co.in/delici/static/media/featured-2.c5352547bb6b76b344a7.jpg' />
              
